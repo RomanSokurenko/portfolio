166c249101388d1983ce03d52979d3e0190d6e0d
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-11-28 10:43:41 +0200

    js поправил

--- js/main.js
+++ js/main.js
@@ -1,17 +1,9 @@
-// document.querySelector(".burger__btn").addEventListener("click", function (e) {
-//   e.preventDefault();
-//   this.classList.toggle(".is-active");
-// });
 $(function () {
   $(".burger__btn").on("click", function () {
     $(".burger__btn").toggleClass("is-active");
   });
-});
-
-$(function () {
-  $(".menu").on("click", function () {
+  $(".burger__btn").on("click", function () {
     $(".menu").toggleClass("menu--active");
   });
-
   var mixer = mixitup(".content");
 });
