2bed13cfcf17361ca3fed3ab73057e9bcfb3640a
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-11-28 10:26:53 +0200

    save адаптив 50%

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
+// document.querySelector(".burger__btn").addEventListener("click", function (e) {
+//   e.preventDefault();
+//   this.classList.toggle(".is-active");
+// });
 $(function () {
-  $(".burger").on("click", function () {
+  $(".burger__btn").on("click", function () {
+    $(".burger__btn").toggleClass("is-active");
+  });
+});
+
+$(function () {
+  $(".menu").on("click", function () {
     $(".menu").toggleClass("menu--active");
   });
 
